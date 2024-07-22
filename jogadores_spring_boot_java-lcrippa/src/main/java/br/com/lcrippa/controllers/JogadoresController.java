c11f059e503a2ce9e73e32bd0a4bbe71b170c39a
Lucas <lucas_crippa@hotmail.com>
2024-07-22 14:58:40 -0300

    Atualização

--- jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/controllers/JogadoresController.java
+++ jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/controllers/JogadoresController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,15 @@ public class JogadoresController {
 	@RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Jogadores>findAll(){
 		return service.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Jogadores findById(@PathVariable(value = "id") String id) {
+		return service.findById(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
+	public Jogadores create(@RequestBody Jogadores jogador) {
+		return service.create(jogador);
 	}
 }
--- jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/services/JogadoresServices.java
+++ jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/services/JogadoresServices.java
@@ -12,26 +12,49 @@ import br.com.lcrippa.model.Jogadores;
 public class JogadoresServices {
 
 	private final AtomicLong counter = new AtomicLong();
-	
-	public List<Jogadores> findAll(){
-		List<Jogadores> jogadores= new ArrayList<>();
-		for(int i=0; i<=10;i++) {
-			Jogadores jogador1 = mockJogadores(i);
-			jogadores.add(jogador1);	
-			
-		} return jogadores;
+
+	public Jogadores findById(String id) {
+		Jogadores jogador = new Jogadores();
+		jogador.setId(counter.incrementAndGet());
+		jogador.setNome("Leandro");
+		jogador.setSobrenome("Costa");
+		jogador.setPosicao("Atacante");
+		jogador.setCPF("4578954");
+		jogador.setDt_nascimento("01/01/1991");
+		return jogador;
 	}
-	
-	
+
+	public List<Jogadores> findAll() {
+		List<Jogadores> jogadores = new ArrayList<>();
+		for (int i = 0; i <= 10; i++) {
+			Jogadores jogador1 = mockJogadores(i);
+			jogadores.add(jogador1);
+
+		}
+		return jogadores;
+	}
+
 	private Jogadores mockJogadores(int i) {
 		Jogadores jogador = new Jogadores();
 		jogador.setId(counter.incrementAndGet());
 		jogador.setNome("Person name" + i);
 		jogador.setSobrenome("Last Name " + i);
-		jogador.setCPF("123456 " +i);
+		jogador.setCPF("123456 " + i);
 		jogador.setDt_nascimento("01/31/2" + i);
 		jogador.setPosicao("Atacante");
 		return jogador;
 	}
+
+	public Jogadores create(Jogadores jogador) {
+		return jogador;
+	}
+
+	public Jogadores update(Jogadores jogador) {
+		return jogador;
+	}
 	
+	
+	public void delete(String id) {
+		
+	}
 }
