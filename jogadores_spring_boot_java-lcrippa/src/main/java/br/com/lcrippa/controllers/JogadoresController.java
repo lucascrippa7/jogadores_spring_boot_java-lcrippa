063e4928ab37dd0d80fa32ddb425b2c19b93d712
Lucas <lucas_crippa@hotmail.com>
2024-08-01 02:20:51 -0300

    01/08/2024

--- jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/controllers/JogadoresController.java
+++ jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/controllers/JogadoresController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lcrippa.model.Jogadores;
@@ -21,23 +25,30 @@ public class JogadoresController {
 	@Autowired
 	private JogadoresServices service;
 
-	@RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Jogadores>findAll(){
 		return service.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Jogadores findById(@PathVariable(value = "id") String id) {
+	@GetMapping(value = "/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
+	public Jogadores findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces =MediaType.APPLICATION_JSON_VALUE)
 	public Jogadores create(@RequestBody Jogadores jogador) {
 		return service.create(jogador);
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") String id) {
-		service.delete(id);
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Jogadores update(@RequestBody Jogadores jogador) {
+		return service.update(jogador);
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();	
+		}
 }
--- jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/services/JogadoresServices.java
+++ jogadores_spring_boot_java-lcrippa/src/main/java/br/com/lcrippa/services/JogadoresServices.java
@@ -1,60 +1,55 @@
 package br.com.lcrippa.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.lcrippa.exception.ResourceNotFoundException;
 import br.com.lcrippa.model.Jogadores;
+import br.com.lcrippa.repositories.JogadoresRepository;
 
 @Service
 public class JogadoresServices {
 
-	private final AtomicLong counter = new AtomicLong();
+	@Autowired
+	JogadoresRepository repository;
 
-	public Jogadores findById(String id) {
-		Jogadores jogador = new Jogadores();
-		jogador.setId(counter.incrementAndGet());
-		jogador.setNome("Leandro");
-		jogador.setSobrenome("Costa");
-		jogador.setPosicao("Atacante");
-		jogador.setCPF("4578954");
-		jogador.setDt_nascimento("01/01/1991");
-		return jogador;
+	public Jogadores findById(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 	}
 
 	public List<Jogadores> findAll() {
-		List<Jogadores> jogadores = new ArrayList<>();
-		for (int i = 0; i <= 10; i++) {
-			Jogadores jogador1 = mockJogadores(i);
-			jogadores.add(jogador1);
 
-		}
-		return jogadores;
-	}
-
-	private Jogadores mockJogadores(int i) {
-		Jogadores jogador = new Jogadores();
-		jogador.setId(counter.incrementAndGet());
-		jogador.setNome("Person name" + i);
-		jogador.setSobrenome("Last Name " + i);
-		jogador.setCPF("123456 " + i);
-		jogador.setDt_nascimento("01/31/2" + i);
-		jogador.setPosicao("Atacante");
-		return jogador;
+		return repository.findAll();
 	}
 
 	public Jogadores create(Jogadores jogador) {
-		return jogador;
+		return repository.save(jogador);
 	}
 
 	public Jogadores update(Jogadores jogador) {
-		return jogador;
+
+		var entity = repository.findById(jogador.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setNome(jogador.getNome());
+		entity.setSobrenome(jogador.getSobrenome());
+		entity.setCPF(jogador.getCPF());
+		entity.setDt_nascimento(jogador.getDt_nascimento());
+		entity.setPosicao(jogador.getPosicao());
+		return repository.save(jogador);
 	}
+
 	
-	
-	public void delete(String id) {
+
+	public void delete(Long id) {
 		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		repository.delete(entity);
+
 	}
 }
